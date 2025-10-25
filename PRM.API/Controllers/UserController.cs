241b0e0886e54083eddb129e72f2a5cf746844ca
huynhcongminhtrise183799 <trihcmse183799@fpt.edu.vn>
2025-10-25 16:43:55 +0700

    Upload function add fcm token

--- PRM.API/Controllers/UserController.cs
+++ PRM.API/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PRM.API.DTOs.Request;
 using PRM.Application.IService;
+using PRM.Application.Model;
 
 namespace PRM.API.Controllers
 {
@@ -9,14 +11,27 @@ namespace PRM.API.Controllers
 	public class UserController : ControllerBase
 	{
 		private readonly IUserService _userService;
-		public UserController(IUserService userService)
+		private readonly IUserDeviceTokenService _userDeviceTokenService;
+		public UserController(IUserService userService, IUserDeviceTokenService userDeviceTokenService)
 		{
 			_userService = userService;
+			_userDeviceTokenService = userDeviceTokenService;
 		}
 		[HttpGet("admin")]
 		public async Task<IActionResult> GetAdmin()
 		{
 			var result = await _userService.GetAdmin();
+			return Ok(result);
+		}
+		[HttpPost("user/device-token")]
+		public async Task<IActionResult> AddUserDeviceToken([FromBody] UserDeviceTokenRequest userDeviceToken)
+		{
+			var model = new UserDeviceTokenModel
+			{
+				UserId = userDeviceToken.UserId,
+				FCMToken = userDeviceToken.FCMToken,
+			};
+			var result = await _userDeviceTokenService.AddToken(model);
 			return Ok(result);
 		}
 	}
---
+++ PRM.Application/IService/IUserDeviceTokenService.cs
@@ -0,0 +1,15 @@
+﻿using PRM.Application.Model;
+using PRM.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRM.Application.IService
+{
+    public interface IUserDeviceTokenService
+    {
+        Task<bool> AddToken(UserDeviceTokenModel userDeviceToken);
+    }
+}
